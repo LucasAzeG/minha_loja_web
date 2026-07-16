ba114770d5ede82d016c347234f5c6c4db6637f0
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-16 14:54:25 -0300

    sla., to tentando

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,6 +1,9 @@
 //CRIANDO O ARRAY DE ITENS DO CARRINH0
 const itensCarrinho = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
 
+//IMPORTANDO LISTA_PRODUTOS
+import {produtos} from './lista_produtos.js'
+
 //const itensCarrinho = JSON.parse(localStorage.get('carrinhoSessao')) || []
 
 //FUNÇÃO ITEM
@@ -8,7 +11,7 @@ const item = (objProduto) => {
 
     const item = {
         id_produto: objProduto.id_produto,
-        descricao_produto: objProduto.descricao_produto,
+        nome: objProduto.nome,
         valor_unitario: objProduto.valor_unitario,
         unidade: objProduto.unidade,
         caminho_imagem: objProduto.caminho_imagem,
@@ -52,27 +55,3 @@ const removeItem = (pos) =>{
 
 //EXPORTAÇÃO
 export { addItem, listItens, removeItem }
-
-
-tela_carrinho.js:
-
-const aRemover = document.createElement('a')
-        aRemover.setAttribute('href','#')
-        aRemover.setAttribute('class', 'lnk-remover')
-        aRemover.innerHTML = `Remover`
-
-        aRemover.addEventListener('click',()=>{
-            if(confirm(`Tem certeza que deseja remover ${elem.descricao_produto}`)){
-                removeItemTela(i)
-            }
-        })
-
-        const imgRemover = document.createElement('img')
-        imgRemover.setAttribute('src', '../imagens/icone/remover.png')
-        imgRemover.setAttribute('alt', 'remover')
-
-
-        imgRemover.addEventListener('click', () => {
-            removeItem(i);
-            montaTelaCarrinho();
-        });
